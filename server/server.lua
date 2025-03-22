ca75b196de16d794395262904be9355f5ec0729f
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-22 16:39:18 -0500

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,43 +1,47 @@
 XYZ = exports['qb-core']:GetCoreObject()
 
 AddEventHandler('onResourceStart', function(resource)
-   if resource == GetCurrentResourceName() then
-      print('====================================')
-      print('           xyz_spawnmenu 1.0.4      ')
-      print('====================================')
-   end
+    if resource == GetCurrentResourceName() then
+        -- Removed console logging
+    end
 end)
 
----@param cid string
----@return table?
-lib.callback.register('qb-spawn:server:getOwnedHouses', function(_, cid)
-   if cid ~= nil then
-      local houses = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', {cid})
-       if houses[1] ~= nil then
-           return houses
-       end
-   end
-end)
+local function hasApartment(cid)
+    if not cid or type(cid) ~= "string" then return false end
+    return MySQL.single.await('SELECT * FROM properties WHERE owner_citizenid = ? LIMIT 1', {cid}) ~= nil
+end
 
 lib.callback.register('xyz_spawn:GetOwnedApartment', function(source, cid)
-   local Player = XYZ.Functions.GetPlayer(source)
-   local result = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', { cid })
+    local Player = XYZ.Functions.GetPlayer(source)
+    if not Player then
+        return false
+    end
 
-   if not cid then
-       cid = Player.PlayerData.citizenid
-   end
+    cid = cid or Player.PlayerData.citizenid
+    if hasApartment(cid) then
+        return MySQL.single.await('SELECT * FROM properties WHERE owner_citizenid = ? LIMIT 1', {cid})
+    else
+        return false
+    end
+end)
 
-   if result[1] then
-      lib.print.info('XYZ-Spawn: Player ' .. cid .. ' has ' .. #result .. ' apartments')
-       return result[1]
-   end
+lib.callback.register('qb-spawn:server:getOwnedHouses', function(_, cid)
+    if not cid then return nil end
+    local houses = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', {cid})
+    return houses[1] and houses or nil
 end)
 
 lib.callback.register('xyz_spawnmenu:server:updateUI', function(source)
-   local Player = XYZ.Functions.GetPlayer(source)
-   local userinfo = {
-       name = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname,
-       job = Player.PlayerData.job.label,
-   }
-   return userinfo
+    local Player = XYZ.Functions.GetPlayer(source)
+    if not Player then
+        return {
+            name = 'Unknown',
+            job = 'Unemployed'
+        }
+    end
+
+    return {
+        name = (Player.PlayerData.charinfo.firstname or 'Unknown') .. ' ' .. (Player.PlayerData.charinfo.lastname or ''),
+        job = (Player.PlayerData.job and Player.PlayerData.job.label) or 'Unemployed'
+    }
 end)
