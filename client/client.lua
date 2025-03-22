f080b99db7269b8b671b7d8b70892a765a832484
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-22 16:44:43 -0500

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,55 +1,50 @@
-local Core = exports['qb-core']:GetCoreObject() lib.locale()
+local Core = exports['qb-core']:GetCoreObject()
+lib.locale()
 
 local function toggleNuiFrame(shouldShow)
-    local userinfo = lib.callback.await('xyz_spawnmenu:server:updateUI', false, source)
+    local userinfo = lib.callback.await('xyz_spawnmenu:server:updateUI', false) or { name = "Unknown", job = "Unemployed" }
+    
     SetNuiFocus(shouldShow, shouldShow)
     SendReactMessage('setVisible', shouldShow)
     SendReactMessage('userinfo', userinfo)
 end
 
-
-RegisterNetEvent('apartments:client:setupSpawnUI', function(cData , new)
-    TriggerEvent('xyz-weather:client:SpawnMenu', source)
+RegisterNetEvent('apartments:client:setupSpawnUI', function(cData)
     local result = lib.callback.await('xyz_spawn:GetOwnedApartment', false, cData.citizenid)
+
     TriggerScreenblurFadeIn(500)
-    if result then
-        SwitchOutPlayer(cache.ped, 0, 1)
-        while GetPlayerSwitchState() ~= 5 do Wait(0) end
-        DoScreenFadeIn(1000)
-        toggleNuiFrame(true)
-        SendReactMessage('IsNew', false)
-    else
-        SwitchOutPlayer(cache.ped, 0, 1)
-        while GetPlayerSwitchState() ~= 5 do Wait(0) end
-        DoScreenFadeIn(1000)
-        toggleNuiFrame(true)
-        SendReactMessage('IsNew', true)
-    end
+    SwitchOutPlayer(cache.ped, 0, 1)
+
+    while GetPlayerSwitchState() ~= 5 do Wait(0) end
+
+    DoScreenFadeIn(1000)
+    toggleNuiFrame(true)
+    SendReactMessage('IsNew', not result or type(result) ~= "table")
 end)
 
-
-RegisterNetEvent('qb-spawn:client:openUI', function(value)
+RegisterNetEvent('qb-spawn:client:openUI', function()
     SetNuiFocus(true, true)
     SetEntityVisible(cache.ped, false)
     DoScreenFadeOut(250)
+
     while GetPlayerSwitchState() ~= 5 do Wait(0) end
     DoScreenFadeIn(1000)
-    Core.Functions.GetPlayerData(function(PlayerData)end)
-    Wait(500)
-    TriggerEvent('apartments:client:setupSpawnUI')
+
+    Core.Functions.GetPlayerData(function(PlayerData)
+        TriggerEvent('apartments:client:setupSpawnUI', PlayerData)
+    end)
+
     toggleNuiFrame(true)
-    TriggerEvent('xyz-weather:client:EnableSync', src)
+    TriggerEvent('xyz-weather:client:EnableSync')
 end)
 
 RegisterNUICallback('hideFrame', function(_, cb)
-    SetNuiFocus(false, false)
     toggleNuiFrame(false)
     cb('ok')
 end)
 
 RegisterNUICallback('forge-spawn', function(data, cb)
-    local index = tonumber(data.index)
-    local ped = cache.ped
+    if not data or not data.coords then return cb('error') end
     local spawnLocation = vector4(data.coords.x, data.coords.y, data.coords.z, data.coords.h)
 
     toggleNuiFrame(false)
@@ -59,50 +54,38 @@ RegisterNUICallback('forge-spawn', function(data, cb)
     cb('ok')
 end)
 
-
-RegisterNUICallback('forge-navbar', function(data, cb)
+RegisterNUICallback('forge-navbar', function(_, cb)
     PlaySoundFrontend(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
     cb('ok')
 end)
 
-RegisterNUICallback('logout', function(data, cb)
+RegisterNUICallback('logout', function(_, cb)
     cb('ok')
 end)
 
-RegisterNUICallback('forge-introplayer', function(data, cData, new, cb)
-    local ped = cache.ped
+RegisterNUICallback('forge-introplayer', function(_, cb)
     toggleNuiFrame(false)
-
     DoScreenFadeOut(0)
-
-    SwitchInPlayer(ped)
+    SwitchInPlayer(cache.ped)
 
     while GetPlayerSwitchState() ~= 12 do Wait(0) end
-
-    CreateCam(source)
+    CreateCam(cache.ped)
+    cb('ok')
 end)
 
 function spawnPlayer(spawnLocation)
-    local ped = cache.ped
+    if not spawnLocation then return end
 
-    SetEntityVisible(cache.ped, true)
+    local ped = cache.ped
+    SetEntityVisible(ped, true)
     RequestCollisionAtCoord(spawnLocation.x, spawnLocation.y, spawnLocation.z)
     SetEntityCoordsNoOffset(ped, spawnLocation.x, spawnLocation.y, spawnLocation.z, false, false, false, true)
     SetEntityHeading(ped, spawnLocation.h)
     FreezeEntityPosition(ped, true)
 
-    SetGameplayCamRelativeHeading(0)
-
-    while GetPlayerSwitchState() ~= 5 do Wait(0) end
-
-    SwitchInPlayer(ped)
-
-    while GetPlayerSwitchState() ~= 12 do Wait(0) end
-
     while not HasCollisionLoadedAroundEntity(ped) do Wait(0) end
-    SetEntityCoordsNoOffset(ped, spawnLocation.x, spawnLocation.y, spawnLocation.z, false, false, false, true)
 
-	FreezeEntityPosition(ped, false)
+    FreezeEntityPosition(ped, false)
 
     TriggerServerEvent('ps-housing:server:resetMetaData')
     TriggerEvent('xyz-weather:client:EnableSync')
@@ -110,54 +93,26 @@ function spawnPlayer(spawnLocation)
     TriggerEvent('QBCore:Client:OnPlayerLoaded')
 end
 
-RegisterNUICallback('lastlocation', function(data, cb)
+RegisterNUICallback('lastlocation', function(_, cb)
     TriggerScreenblurFadeOut(0)
-    local PlayerData = Core.Functions.GetPlayerData()
-    local insideMeta = PlayerData.metadata["inside"]
-
-    -- xSound:Destroy('intro')
-    toggleNuiFrame(false)
 
     Core.Functions.GetPlayerData(function(pd)
-        ped = cache.ped
+        if not pd or not pd.position then return cb('error') end
 
-        SetEntityVisible(cache.ped, true)
-        RequestCollisionAtCoord(pd.position.x, pd.position.y, pd.position.z, - 1.0)
+        local ped = cache.ped
+        local insideMeta = pd.metadata and pd.metadata["inside"]
+
+        SetEntityVisible(ped, true)
+        RequestCollisionAtCoord(pd.position.x, pd.position.y, pd.position.z, -1.0)
         SetEntityCoords(ped, pd.position.x, pd.position.y, pd.position.z - 2.0)
         SetEntityHeading(ped, pd.position.a)
 
         FreezeEntityPosition(ped, true)
-        SetGameplayCamRelativeHeading(0)
-
-        while GetPlayerSwitchState() ~= 5 do Wait(0) end
-
-        SwitchInPlayer(ped)
-
-        while GetPlayerSwitchState() ~= 12 do Wait(0) end
 
         while not HasCollisionLoadedAroundEntity(ped) do Wait(0) end
 
-
-        local dict = "anim@scripted@heist@ig25_beach@male@"
-        RequestAnimDict(dict)
-        repeat Wait(0) until HasAnimDictLoaded(dict)
-
-        local playerPed = PlayerPedId()
-        local playerPos = GetEntityCoords(playerPed)
-        local playerHead = GetEntityHeading(playerPed)
-
-        local scene = NetworkCreateSynchronisedScene(playerPos.x, playerPos.y, playerPos.z, 0.0, 0.0, playerHead, 2, false, false, 8.0, 1000.0, 1.0)
-        NetworkAddPedToSynchronisedScene(playerPed, scene, dict, "action", 1000.0, 8.0, 0, 0, 1000.0, 8192)
-        NetworkAddSynchronisedSceneCamera(scene, dict, "action_camera")
-        NetworkStartSynchronisedScene(scene)
-
-        Wait(GetAnimDuration(dict, "action") * 1000)
-
-        FreezeEntityPosition(ped, false)
-
-        if insideMeta.property_id ~= nil then
-            local property_id = insideMeta.property_id
-            TriggerServerEvent('ps-housing:server:enterProperty', tostring(property_id))
+        if insideMeta and insideMeta.property_id then
+            TriggerServerEvent('ps-housing:server:enterProperty', tostring(insideMeta.property_id))
         else
             TriggerServerEvent('ps-housing:server:resetMetaData')
         end
@@ -165,6 +120,7 @@ RegisterNUICallback('lastlocation', function(data, cb)
         TriggerServerEvent('QBCore:Server:OnPlayerLoaded')
         TriggerEvent('QBCore:Client:OnPlayerLoaded')
         TriggerEvent('xyz-weather:client:EnableSync')
+
+        cb('ok')
     end)
-    cb('ok')
 end)
